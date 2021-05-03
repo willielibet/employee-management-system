25ee0dc54e6c36c040b4244235e402933e93f50d
Willie <write2willie@gmail.com>
2021-05-02 21:36:43 -0400

    commit #4: fixed syntax error on sql for department table, may022021, 9:36pm

--- db_schemas.sql
+++ db_schemas.sql
@@ -1,18 +1,10 @@
---creates a db schema called employees 
-CREATE DATABASE employees_db;
 
---makes it so all of the following code will affect employees_db  --
-USE employees_db;
+CREATE DATABASE employeesDB;
 
---creates the table "deparment" within employees_db.
---given the relationship among the tables in this schema, the
---department table needs to be created first since
---it is the only table without a FK.
-CREATE TABLE deparment (
---creates a numeric column called "id" which automatically 
---increments and cannot be null.
+USE employeesDB;
+
+CREATE TABLE department (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  --makes a string column called "name" containing names of 
-  --up to 30 characters.
-  name VARCHAR(30)
+  name VARCHAR(30),
+  PRIMARY KEY (id)
 );
