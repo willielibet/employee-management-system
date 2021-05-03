13d406bb09b4df2d5a5fc251ac8c98a43cdba76c
Willie <write2willie@gmail.com>
2021-05-03 08:42:19 -0400

    commit #7: taking connection file out of gitignore, may032021, 8:42am

---
+++ employeesDBConnection.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const util = require("util");
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSSWORD,
+  database: process.env.DB_DATABASE
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  connection.end();
+});
+
+module.exports = connection;
+
+
